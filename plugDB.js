040004d3140edc42fb6b7ab9c03c3adad1290a0c
pbxx <airlew@gmail.com>
2023-01-25 11:22:57 -0700

    convert to using plugdb-postgres module

--- plugDB.js
+++ plugDB.js
@@ -14,19 +14,7 @@ module.exports = {
 
       if (options.mode) {
         //the mode must be set so adrauth knows what type of database to use
-        if (options.mode == "postgres") {
-          var { DBLink } = require("./connectors/postgres.js");
-          //spawn a new Postgres DBLink
-          this.actions = new DBLink(options, (err, resp) => {
-            if (err) {
-              callback(err);
-            } else {
-
-              callback(null, resp);
-            }
-          });
-
-        } else if (options.mode == "single-json-file") {
+        if (options.mode == "single-json-file") {
           var { DBLink } = require("./connectors/singleJSON.js");
           //under construction
 
